5fa568a642e34f0408f6fbd1d30c3020206909ec
chumenium <ovs236118@gmail.com>
2025-06-27 12:22:47 +0900

    DBConnectionクラスの構文エラーを修正：import文の分離とJavaDocコメントの追加

--- src/main/java/utils/DBConnection.java
+++ src/main/java/utils/DBConnection.java
@@ -1,16 +1,26 @@
-package utils; import java.sql.Connection;
+package utils;
+
+import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException; 
+import java.sql.SQLException;
 
-
-//PCが変わるたび変更をすること
+/**
+ * データベース接続ユーティリティクラス
+ * PCが変わるたび変更をすること
+ */
 public class DBConnection {
-  private static final String URL = "jdbc:mysql://localhost:3306/jms";
-  private static final String USER = "root";
-  private static final String PASSWORD = "kcsf";
+    private static final String URL = "jdbc:mysql://localhost:3306/jms";
+    private static final String USER = "root";
+    private static final String PASSWORD = "kcsf";
 
-  public static Connection getConnection() throws SQLException, ClassNotFoundException {
-      Class.forName("com.mysql.cj.jdbc.Driver");
-      return DriverManager.getConnection(URL, USER, PASSWORD);
-  }
+    /**
+     * データベース接続を取得する
+     * @return Connection データベース接続オブジェクト
+     * @throws SQLException SQL例外
+     * @throws ClassNotFoundException クラスが見つからない例外
+     */
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
